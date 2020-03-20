7a752c5d3be2c35ceb68a52e22353b881ddf704a
chenxu-yang <cy2554@columbia.edu>
2020-03-20 13:09:48 -0400

    deploy

---
+++ build/precache-manifest.665c281dec936e223308ddff7649a721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f379726c03c2fd9db28d2d082801bc",
+    "url": "/RobotFriend/index.html"
+  },
+  {
+    "revision": "6b278eff10e6a172c0af",
+    "url": "/RobotFriend/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "bb452b1e2eeb44d5cbf2",
+    "url": "/RobotFriend/static/css/main.167749a2.chunk.css"
+  },
+  {
+    "revision": "6b278eff10e6a172c0af",
+    "url": "/RobotFriend/static/js/2.06a64376.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/RobotFriend/static/js/2.06a64376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb452b1e2eeb44d5cbf2",
+    "url": "/RobotFriend/static/js/main.0688b66c.chunk.js"
+  },
+  {
+    "revision": "3ec27775efd5b8b14574",
+    "url": "/RobotFriend/static/js/runtime-main.7e01d1b2.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2182324d1953c9d997777545415ef787.js"
+  "/RobotFriend/precache-manifest.665c281dec936e223308ddff7649a721.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RobotFriend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
