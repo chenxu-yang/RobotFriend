571e43f296280cc7ea00643ce11caf7d6e560a0e
chenxu-yang <cy2554@columbia.edu>
2020-03-20 13:21:45 -0400

    deployed

--- build/precache-manifest.8252a5ffd5f6374f150adbcf8af2e1df.js
+++ build/precache-manifest.8252a5ffd5f6374f150adbcf8af2e1df.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "00f379726c03c2fd9db28d2d082801bc",
+    "revision": "9336bb8df06a01ed6c944425aecf4f1d",
     "url": "/RobotFriend/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/RobotFriend/static/css/2.3b6f1bda.chunk.css"
   },
   {
-    "revision": "bb452b1e2eeb44d5cbf2",
+    "revision": "683fba003aba55da8ee1",
     "url": "/RobotFriend/static/css/main.167749a2.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/RobotFriend/static/js/2.06a64376.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bb452b1e2eeb44d5cbf2",
-    "url": "/RobotFriend/static/js/main.0688b66c.chunk.js"
+    "revision": "683fba003aba55da8ee1",
+    "url": "/RobotFriend/static/js/main.4d4ae7b9.chunk.js"
   },
   {
     "revision": "3ec27775efd5b8b14574",
